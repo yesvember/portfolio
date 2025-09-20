8b80571613067be230f74289fe89682468920923
mewji <syj981118@naver.com>
2025-09-21 01:54:27 +0900

    5

--- script/index.js
+++ script/index.js
@@ -65,16 +65,60 @@ gsap.fromTo('.intro__summary-img > span', {
 })
 
 gsap.fromTo('.intro__img-wrap > img', 
-  { y: -20, scale: 1.1 }, 
-  { y: 20, scale: 1.1,
+  { y: -60, scale: 1.2 }, 
+  { y: 0, scale: 1,
+
     scrollTrigger: {
       trigger: ".intro__contents",
       start: "center bottom",
-      end: "bottom bottom",
+      end: "200% bottom",
       scrub: 1
     }
   }
 );
+
+// skills scrolltrigger
+gsap.fromTo('.skills-deco', 
+  { y: 0}, 
+  { y: 200,
+
+    scrollTrigger: {
+      trigger: ".section-skills",
+      start: "80% bottom",
+      end: "200% bottom",
+      scrub: 1
+    }
+  }
+);
+
+// work scrolltrigger
+gsap.fromTo('.work__intro-left img', 
+  { y: -60, scale: 1.2 }, 
+  { y: 0, scale: 1,
+
+    scrollTrigger: {
+      trigger: ".work__intro",
+      start: "20% bottom",
+      end: "200% bottom",
+      scrub: 1
+    }
+  }
+);
+
+gsap.fromTo('.work-deco', 
+  {rotate : 360}, 
+  {rotate:0,
+    
+    scrollTrigger: {
+      trigger: ".work__intro",
+      start: "20% bottom",
+      end: "200% bottom",
+      scrub: 1
+    }
+  }
+);
+
+
 
 
 // end scrolltigger
@@ -123,14 +167,48 @@ gsap
   .fromTo(".contact__text__ani-01", { xPercent: 400, opacity : 0}, { xPercent: 0, opacity : 1 })
   .fromTo(".contact__text__ani-02", { xPercent: -400, opacity : 0}, { xPercent: 0, opacity : 1 }, "<");
 
+
 // swiper
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".swiper__01", {
   direction: "horizontal",
   loop: false,
   spaceBetween: 10,
-  autoplay: {
+   autoplay: {
     delay: 4000,
     disableOnInteraction: false,
+    pauseOnMouseEnter: true, 
+  },
+
+    navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+  
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1, 
+    },
+    481: {
+      slidesPerView: 1,
+    },
+    1025: {
+      slidesPerView: 2,
+    },
+    1400: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".swiper__02", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 10,
+   autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true, 
   },
 
   breakpoints: {
@@ -138,18 +216,42 @@ const swiper = new Swiper(".swiper", {
       slidesPerView: 1, 
     },
     481: {
-      slidesPerView: 1.1,
+      slidesPerView: 1,
     },
     1025: {
-      slidesPerView: 2.1,
+      slidesPerView: 2,
     },
     1400: {
-      slidesPerView: 3.1,
+      slidesPerView: 2,
     },
   },
-
-   on: {
-    init: () => ScrollTrigger.refresh(),
-    resize: () => ScrollTrigger.refresh(),
-  }
 });
+
+
+const swiper3 = new Swiper(".swiper__03", {
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 10,
+   autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true, 
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerView: 1, 
+    },
+    481: {
+      slidesPerView: 1,
+    },
+    1025: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+
